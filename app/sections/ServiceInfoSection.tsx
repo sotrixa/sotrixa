c475d7a496819d44e223f46fcdf36ab0e83179bd
Mnstanchev <metody.stanchev@netinsky.com>
2025-05-21 15:57:02 +0300

    Fixing the Services in ServiceInfoSection

--- app/sections/ServiceInfoSection.tsx
+++ app/sections/ServiceInfoSection.tsx
@@ -999,11 +999,11 @@ export default function ServiceInfoSection({ onBackClick, activeService: initial
 			{/* Particles container for animated effects */}
 			<div ref={particlesRef} className='absolute inset-0 pointer-events-none z-10'></div>
 
-			<div ref={sectionDivRef} className='flex flex-col md:flex-row w-full h-full relative z-20 mx-auto my-8'>
+			<div ref={sectionDivRef} className='flex flex-col md:flex-row w-full h-full relative z-20 mx-auto my-8 mt-0'>
 				{/* Left side with strategy heading and services list */}
-				<div className='w-full md:w-1/2 p-4 md:p-6' ref={leftSideRef}>
-					<div className='mb-4 sticky top-5 z-50' ref={logoRef}>
-						<button onClick={handleBackToServices} ref={backButtonRef} className='group flex items-center space-x-2 cursor-pointer bg-white/80 backdrop-blur px-3 py-2 rounded-lg shadow-sm hover:shadow'>
+				<div className='w-full md:w-1/2 p-4 md:p-6 mt-10' ref={leftSideRef}>
+					<div className='mb-6' ref={logoRef}>
+						<button onClick={handleBackToServices} ref={backButtonRef} className='group flex items-center space-x-2 cursor-pointer text-black hover:text-[#f4dd65] transition-colors duration-300'>
 							<svg xmlns='http://www.w3.org/2000/svg' className='h-5 w-5 transform transition-transform duration-300 group-hover:-translate-x-1' viewBox='0 0 20 20' fill='currentColor'>
 								<path fillRule='evenodd' d='M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l4.293 4.293a1 1 0 010 1.414z' clipRule='evenodd' />
 							</svg>
@@ -1011,7 +1011,7 @@ export default function ServiceInfoSection({ onBackClick, activeService: initial
 						</button>
 					</div>
 
-					<div className='my-6 mt-16'>
+					<div className='mb-6'>
 						<h1 className='text-3xl md:text-4xl font-bold mb-4' ref={headingRef}>
 							<span className='text-transparent bg-clip-text bg-gradient-to-r from-[#f4dd65] via-[#d142e2] to-[#70DFC6]'>{currentContent.title}</span>
 						</h1>
